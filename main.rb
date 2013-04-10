a2dea271e575af0ad96d826f7bb3ead8a5a1aed6
Zuyang Kou <leafduo@gmail.com>
2013-04-11 01:29:55 +0800

    Add retry and some logging

--- main.rb
+++ main.rb
@@ -19,8 +19,23 @@ end
 log.info filters
 
 links_to_add = []
-Feedzirra::Feed.add_common_feed_entry_element(:enclosure, :value => :url, :as => :enclosure_url) 
-feed = Feedzirra::Feed.fetch_and_parse('http://share.dmhy.org/topics/rss/rss.xml')
+Feedzirra::Feed.add_common_feed_entry_element(:enclosure, :value => :url, :as => :enclosure_url)
+
+retry_count = 0
+feed = nil
+loop do
+    log.info 'Try fetching....'
+    feed = Feedzirra::Feed.fetch_and_parse('http://share.dmhy.org/topics/rss/rss.xml')
+    retry_count += 1
+    break if retry_count > 3 or feed.is_a? Feedzirra::Parser::RSS
+end
+
+if feed.is_a? Feedzirra::Parser::RSS
+    log.info 'Fetched'
+else
+    log.error 'Failed fetching'
+end
+
 feed.entries.each do |entry|
     if filters.any? { |filter| entry.title =~ filter }
         puts entry.title
@@ -32,4 +47,6 @@ if not links_to_add.empty?
     links_to_add.map! { |link| "\"#{link}\""}
     links_to_add = links_to_add.join(' ')
     exec "xunlei-lixian/lixian_cli.py add --bt #{links_to_add}"
+else
+    log.info 'Nothing to add'
 end
